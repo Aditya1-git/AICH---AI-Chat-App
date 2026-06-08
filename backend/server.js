545c3b223e001ec967c2838b64853d48dfef72c9
Aditya raj <adityaraj5112005@gmail.com>
2026-06-08 17:36:41 +0530

    fix-3 express 5

--- backend/server.js
+++ backend/server.js
@@ -32,11 +32,18 @@ app.use("/api/message", messageRoute);
 app.use("/api/ai", aiRoute);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.use(
+        express.static(
+            path.join(__dirname, "../frontend/dist")
+        )
+    );
 
     app.use((req, res) => {
         res.sendFile(
-            path.join(__dirname, "frontend", "dist", "index.html")
+            path.join(
+                __dirname,
+                "../frontend/dist/index.html"
+            )
         );
     });
 }
