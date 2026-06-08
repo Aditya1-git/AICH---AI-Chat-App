357964931216c42896e094d55b5a2094aca493c2
Aditya raj <adityaraj5112005@gmail.com>
2026-06-08 17:14:59 +0530

    fix express 5 issue in server.js

--- backend/server.js
+++ backend/server.js
@@ -31,12 +31,14 @@ app.use("/api/auth", authRoute);
 app.use("/api/message", messageRoute);
 app.use("/api/ai", aiRoute);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*" , (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend" , "dist" , "index.html"))
-    })
+    app.get("*",(req,res)=>{
+        res.sendFile(
+            path.join(__dirname,"../frontend","dist","index.html")
+        );
+    });
 }
 
 const startServer = async () => {
